8187e004affdc8b7e722901f0efd05e109a008ef
_Gustavo Gresia <gustavogresia22@gmail.com>
2024-12-14 01:48:34 -0300

    creacion de models

--- src/tienda/admin.py
+++ src/tienda/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Comision, Alumno, Curso
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("nombre")
+
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ("comision", "dni")
+
+
+
+@admin.register(Comision)
+class ComisionAdmin(admin.ModelAdmin):
+    list_display = ("curos", "numero", "fecha_de_inicio")
--- src/tienda/models.py
+++ src/tienda/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class  Curso(models.Model):
+    nombre =models.CharField(max_length=255, unique=True)
+
+class Comision(models.Model):
+    Curso =models.ForeignKey(Curso, on_delete= models.SET_NULL, null=True)
+    Numero = models.PositiveIntegerField()
+    Fecha_de_inicio =models.DateField() 
+
+class Alumno(models.Model):
+    Comision =models.ForeignKey(Comision, on_delete= models.SET_NULL, null=True) 
+    Dni = models.IntegerField()
+
+
+    
