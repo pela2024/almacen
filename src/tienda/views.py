e24db9d85d056ce8bbf9ba878e050d1d56c8ceda
_Gustavo Gresia <gustavogresia22@gmail.com>
2024-12-25 21:12:11 -0300

    feat: views update, detail, delete

--- src/tienda/views.py
+++ src/tienda/views.py
@@ -28,9 +28,9 @@ def consorcio_create(request: HttpRequest)-> HttpResponse:
 
 
 
-#### CONSORCIO - UPDATE VIEW 
+#### CONSORCIO - UPDATE VIEWS 
 def consorcio_update(request: HttpRequest, pk:int)-> HttpResponse:
-   query = consorcio.objects.get(id=pk)
+   query = Consorcio.objects.get(id=pk)
    if request.method =="GET":
         form = ConsorcioForm(instance =query) 
    if request.method == "POST":
@@ -38,7 +38,23 @@ def consorcio_update(request: HttpRequest, pk:int)-> HttpResponse:
         if form.is_valid():
             form.save()
             return redirect("tienda:consorcio_list")   
-   return render(request, "tienda/consorcio_form.html", {"form": form})    
+   return render(request, "tienda/consorcio_form.html", {"form": form})
+
+ #### CONSORCIO DETAIL VIEWS
+def consorcio_detail(request: HttpRequest, pk:int)-> HttpResponse:
+   query = Consorcio.objects.get(id=pk)
+   return render(request, "tienda/consorcio_detail.html", {"object": query})
+
+
+   #### CONSORCIO DELETE VIEWS
+def consorcio_delete(request: HttpRequest, pk:int)-> HttpResponse:
+   query = Consorcio.objects.get(id=pk)
+   if request.method == "POST" :
+        query.delete()
+        return redirect ("tienda:consorcio_list")
+   return render(request, "tienda/consorcio_confirm_delete.html", {"object": query})
+
+
 
 
 def Liquidacion_list(request):
