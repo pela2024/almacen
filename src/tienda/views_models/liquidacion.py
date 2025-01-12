7447d3bb0d7410771196053bb767f0a3633a9530
_Gustavo Gresia <gustavogresia22@gmail.com>
2025-01-12 19:46:19 -0300

    feat: diseño del footer

--- src/tienda/views_models/liquidacion.py
+++ src/tienda/views_models/liquidacion.py
@@ -1,7 +1,5 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
-
-
 from .. forms import LiquidacionForm
 from .. models import Liquidacion
 
