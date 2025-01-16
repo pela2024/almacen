0b78422bdacd191af67715710f8cb03f03de6c16
_Gustavo Gresia <gustavogresia22@gmail.com>
2025-01-16 12:37:32 -0300

    feat: cambios en index y login

--- src/tienda/views_models/liquidacion.py
+++ src/tienda/views_models/liquidacion.py
@@ -1,15 +1,32 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
-from .. forms import LiquidacionForm
-from .. models import Liquidacion
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import CreateView
+from .models import  Consorcio, Liquidacion
 
 class  LiquidacionListView(ListView):
     model = Liquidacion
 
+
 class LiquidacionCreateView(CreateView):
-    model= Liquidacion
-    form_class = LiquidacionForm
-    success_url = reverse_lazy ("tienda:liquidacion_list")     
+    model = Liquidacion
+    fields = ['periodo', 'monto']  # Ajusta los campos según tu modelo
+    template_name = 'liquidacion_form.html'
+
+    def get_initial(self):
+        # Captura el consorcio_id desde la URL
+        consorcio_id = self.kwargs.get('consorcio_id')
+        consorcio = get_object_or_404(Consorcio, pk=consorcio_id)
+        
+        # Retorna los valores iniciales para el formulario
+        return {'consorcio': consorcio}
+
+    def form_valid(self, form):
+        # Asigna el consorcio automáticamente antes de guardar
+        consorcio_id = self.kwargs.get('consorcio_id')
+        form.instance.consorcio = get_object_or_404(Consorcio, pk=consorcio_id)
+        return super().form_valid(form)
+  
 
 class LiquidacionUpdateView(UpdateView):
     model= Liquidacion
